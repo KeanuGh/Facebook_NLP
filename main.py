f9f57f9db3a4a6573b5360046e741d48ad5d56a3
keanu <keanu@hotmail.co.uk>
2020-04-25 15:43:49 +0100

    only added start tokens to inputs and end tokens to targets. added creativity

--- main.py
+++ main.py
@@ -7,19 +7,26 @@ def main():
     # import json data into raw dataframe
     json_to_pickle('data_raw.pkl')
     # fix dataframe to be more dataframe-y
-    data = edit_data(input_file='data_raw.pkl', output_file='data.pkl', perform_printouts=True)
+    data = clean_data(input_file='data_raw.pkl', output_file='data.pkl', printouts=True)
 
     # get chat names
     chatnames_df = get_chat_names('data.pkl', to_file=True)
 
-    # check lengths of chatnames
+    # create column containing lengths of chatnames
     chatnames_df['text_length'] = [len(message) for message in chatnames_df['chatname']]
     # chatnames_df['text_length'].plot(kind='hist', bins=30)
     # filter out group names with length > 120
     chatnames_df = chatnames_df[chatnames_df['text_length'] < 120]
 
+    # add start and end tokens to input and output
+    start_token = '\t'
+    end_token = '\n'
+    chatnames_df['inputs'], chatnames_df['targets'] = start_and_end_tokens(chatnames_df['chatname'],
+                                                                           start_token=start_token,
+                                                                           end_token=end_token)
+
     # build vocabulary
-    vocab = build_vocabulary(chatnames_df['chatname'])
+    vocab = build_vocabulary(chatnames_df['chatname'], start_token=start_token, end_token=end_token)
     print(f'vocabulary list: {sorted(list(vocab))}')
     print(f'vocabulary size: {len(vocab)}')
 
@@ -28,22 +35,19 @@ def main():
 
     # build character-to-index dictionaries for vectorisation
     char_to_idx, idx_to_char = char_to_int_maps(vocab)
-
-    # add start and end tokens to input and output
-    chatnames_df['chatnames_processed'] = start_and_end_tokens(chatnames_df['chatname'])
-    inputs, targets = gen_input_and_target(chatnames_df['chatnames_processed'],
+    inputs, targets = gen_input_and_target(chatnames_df['inputs'], chatnames_df['targets'],
                                            vocab=vocab,
                                            seq_length=20,
                                            char_to_idx=char_to_idx,
                                            pickle_filename='tf_dataset.pkl')
 
     # generate model
-    # model = generate_model(vocab, rnn_units=128, seq_len=20)
-    # model = fit_model(model, inputs, targets)
-    model = load_model('model.h5')
-    generate_text(model, 10, 20, vocab, char_to_idx, idx_to_char)
+    model = generate_model(vocab, rnn_units=128, seq_len=20)
+    model = fit_model(model, inputs, targets)
+    # model = load_model('model.h5')
+    generate_text(model, n=10, max_len=120, seq_len=20, vocab=vocab,
+                  char_to_idx=char_to_idx, idx_to_char=idx_to_char)
 
 
 if __name__ == '__main__':
-
     main()
--- nlp.py
+++ nlp.py
@@ -4,12 +4,9 @@ import pandas as pd
 import pickle
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, GRU, Embedding
+from tensorflow.keras.layers import Dense, GRU, Embedding, LSTM
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-
-# disables AVX/FMA warning as I'm using GPU
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
+from tensorflow.keras.optimizers import Adam
 
 
 def start_and_end_tokens(texts, start_token='\t', end_token='\n'):
@@ -21,8 +18,9 @@ def start_and_end_tokens(texts, start_token='\t', end_token='\n'):
     :param end_token: end token
     :return: pandas series containing texts with start and end tokens
     """
-    texts_with_bookends = texts.apply(lambda x: start_token + x + end_token)
-    return texts_with_bookends
+    texts_inputs = texts.apply(lambda x: start_token + x)
+    texts_targets = texts.apply(lambda x: x + end_token)
+    return texts_inputs, texts_targets
 
 
 def build_vocabulary(texts, start_token='\t', end_token='\n'):
@@ -33,7 +31,7 @@ def build_vocabulary(texts, start_token='\t', end_token='\n'):
     :param texts: pandas Series containing all texts
     :return: vocabualary set
     """
-    vocab: set = {start_token, end_token}  # add stop and start tokens
+    vocab = {start_token, end_token}  # add stop and start tokens
     for text in texts:
         for c in text:
             if c not in vocab:
@@ -52,39 +50,48 @@ def char_to_int_maps(vocab):
     return char_to_idx, idx_to_char
 
 
-def gen_input_and_target(text, char_to_idx, vocab: set, seq_length=20, step=1, pickle_filename=None):
+def gen_input_and_target(text_inputs, text_targets, char_to_idx: dict, vocab: set, seq_length: int = 20, step: int = 1,
+                         pickle_filename: str = None):
     """
     TODO
+    :param text_targets:
+    :param text_inputs:
     :param vocab:
     :param step:
-    :param text:
     :param seq_length:
     :param char_to_idx:
     :param pickle_filename:
     :return: dataframe w/ columns 'inputs', 'targets' containing text sequences and their next
     """
     # get max length
-    max_len = text.map(len).max()
-    print(text)
-    # doing the input and targets myself because I am confused
+    max_len = text_inputs.map(len).max()
+
     # create column in dataframe containing vector rep. of characters
-    texts_as_int = [[char_to_idx[c] for c in seq] for seq in text]
+    inputs_as_int = [[char_to_idx[c] for c in seq] for seq in text_inputs]
+    targets_as_int = [[char_to_idx[c] for c in seq] for seq in text_targets]
     # pad sequences to max length (after text)
-    texts_as_int = pad_sequences(texts_as_int, max_len, padding='pre')
+    inputs_as_int = pad_sequences(inputs_as_int, max_len, padding='pre')
+    targets_as_int = pad_sequences(targets_as_int, max_len, padding='pre')
 
     # instantiate lists to contain sequences and next characters
     texts = []
     next_chars = []
     # loop over each text in texts to get subset of length seq_length and the next character
-    for text in texts_as_int:
-        for i in range(0, len(text) - seq_length, step):
-            texts.append(text[i:i + seq_length])
-            next_chars.append(text[i + seq_length])
+    for input_text, target_text in [*zip(inputs_as_int, targets_as_int)]:
+        print(input_text)
+        print(target_text)
+        for i in range(0, len(inputs_as_int) - seq_length, step):
+            texts.append(input_text[i:i + seq_length])
+            next_chars.append(target_text[i + seq_length])
+        print(f"\ntext: {texts[-1]}\ntarget: {next_chars[-1]}")
+
     # create dataframe with these lists ad columns
     ml_data = pd.DataFrame({'inputs': texts, 'targets': next_chars})
+
     # instanciate empty vectors to contain input and target data
     numerical_texts = np.zeros((len(ml_data['inputs']), seq_length + 1, len(vocab)), dtype=bool)
     numerical_next_chars = np.zeros((len(ml_data['targets']), len(vocab)), dtype=bool)
+
     # vectorise imput and targets
     for i, text in enumerate(ml_data['inputs']):
         for t, idx in enumerate(text):
@@ -100,31 +107,30 @@ def gen_input_and_target(text, char_to_idx, vocab: set, seq_length=20, step=1, p
     return numerical_texts, numerical_next_chars
 
 
-def generate_model(vocab, seq_len, rnn_units=50):
+def generate_model(vocab: set, seq_len: int, rnn_units=50, learning_rate=0.0003):
     """
-    TODO
-    :param seq_len:
+    TODO write doc for model generation
+    :param learning_rate:
     :param vocab:
+    :param seq_len:
     :param rnn_units:
-    :param embedding_dim:
     :return:
     """
+    optimizer = Adam(lr=learning_rate)
     model = Sequential()
-    # model.add(Embedding(len(vocab), embedding_dim,
-    #                    mask_zero=True))
-    model.add(GRU(rnn_units, input_shape=(seq_len + 1, len(vocab)),
-                  return_sequences=True,
-                  dropout=0.15, recurrent_dropout=0.15,
-                  recurrent_initializer='glorot_uniform'))
-    model.add(GRU(rnn_units, return_sequences=False,
+    # model.add(Embedding(input_dim=len(vocab), output_dim=20, input_length=seq_len + 1))
+    model.add(LSTM(rnn_units, input_shape=(seq_len + 1, len(vocab)),
+                   return_sequences=True, dropout=0.15, recurrent_dropout=0.15,
+                   recurrent_initializer='glorot_uniform'))
+    model.add(GRU(rnn_units, return_sequences=False, dropout=0.15, recurrent_dropout=0.15,
                   recurrent_initializer='glorot_uniform'))
     model.add(Dense(len(vocab), activation='softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer='adam')
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer)
     print(model.summary())
     return model
 
 
-def fit_model(model, inputs, targets):
+def fit_model(model, inputs, targets, n_epochs: int = 10, batch_size: int = 64):
     # create checkpoints for callbacks
     checkpoint_dir = './training_checkpoints'
     checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
@@ -134,17 +140,37 @@ def fit_model(model, inputs, targets):
 
     # fit model
     history = model.fit(inputs, targets,
-                        epochs=10,
+                        epochs=n_epochs,
                         callbacks=[checkpoint_callback],
-                        batch_size=64,
+                        batch_size=batch_size,
                         verbose=2)
-    model.reset_metrics()
     model.save('model.h5')
+    model.reset_metrics()
     return model
 
 
-def generate_text(model, n: int, max_len: int, vocab: set, char_to_idx: dict, idx_to_char: dict,
-                  start_token: str = '\t', end_token: str = '\n'):
+def generate_text(model, n: int, max_len: int, seq_len: int, vocab: set, char_to_idx: dict, idx_to_char: dict,
+                  start_token: str = '\t', end_token: str = '\n', creativity=1):
+    """
+    TODO: write doc for text generation
+    :param seq_len:
+    :param model:
+    :param n:
+    :param max_len:
+    :param vocab:
+    :param char_to_idx:
+    :param idx_to_char:
+    :param start_token:
+    :param end_token:
+    :param creativity:
+    :return:
+    """
+    # crativity: rescale prediction based on 'temperature'
+    def scale_softmax(softmax_pred, temperatrure=creativity):
+        scaled_pred = np.exp(np.log(softmax_pred) / temperatrure)
+        scaled_pred = scaled_pred / np.sum(scaled_pred)
+        scaled_pred = np.random.multinomial(1, scaled_pred, 1)
+        return np.argmax(scaled_pred)
 
     # generate n texts
     for i in range(0, n):
@@ -153,13 +179,13 @@ def generate_text(model, n: int, max_len: int, vocab: set, char_to_idx: dict, id
         text = ''
 
         # to contain output text, initialise by filling with start character
-        output_seq = np.zeros([1, max_len + 1, len(vocab)])
+        output_seq = np.zeros([1, seq_len + 1, len(vocab)])
         output_seq[0, 0, char_to_idx[start_token]] = 1.
 
         # generate new characters until you reach end token or text reaches maximum length
         while not stop and counter < max_len + 1:
             probs = model.predict_proba(output_seq, verbose=0)[0]
-            c = np.random.choice(sorted(list(vocab)), replace=True, p=probs.reshape(len(vocab)))
+            c = scale_softmax(probs, creativity)
             if c == end_token:
                 stop = True
             else:
